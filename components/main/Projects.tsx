b48e526bf46b744e8007d83e88d05b0ed47327a6
FriedRubyjane <wangjinhyun@gmail.com>
2024-02-26 20:04:49 +0300

    Add My Projects

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,6 +10,7 @@ const Projects = () => {
 			<div className='h-full w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 px-20'>
 				{MyProjects.map(project => (
 					<ProjectsCard
+						key={project.title}
 						src={project.src}
 						title={project.title}
 						description={project.description}
