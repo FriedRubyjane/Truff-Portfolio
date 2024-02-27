61cee58aeaaa8bf438cc9c5386c237a82f30288b
FriedRubyjane <wangjinhyun@gmail.com>
2024-02-27 13:48:35 +0300

    Update

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -5,10 +5,9 @@ const Hero = () => {
 				autoPlay
 				muted
 				loop
+				src='/video/blackHole.webm'
 				className='rotate-180 absolute z-[1] top-[-250px] w-[1080px] h-[650px] object-cover'
-			>
-				<source src='/video/blackHole.webm' type='video/webm' />
-			</video>
+			></video>
 		</section>
 	)
 }
