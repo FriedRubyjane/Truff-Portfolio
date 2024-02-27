4c4456b5ccbfd18ff5423ce38794e0506ba0f1d8
FriedRubyjane <wangjinhyun@gmail.com>
2024-02-27 12:23:46 +0300

    Update

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Navbar = () => {
 	return (
-		<div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50'>
+		<div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001465] backdrop-blur-md z-50'>
 			<div className='w-full h-full flex flex-row items-center justify-between m-auto px-10'>
 				<a
 					href='#about-me'
--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
 			<h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>
 				My Projects
 			</h1>
-			<div className='h-full w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 px-20'>
+			<div className='h-full w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 px-10'>
 				{MyProjects.map(project => (
 					<ProjectsCard
 						key={project.title}
