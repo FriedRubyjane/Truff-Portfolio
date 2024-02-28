b3c2ef1f1a4c32d5fc62f18ba54fe0afb320766a
FriedRubyjane <wangjinhyun@gmail.com>
2024-02-28 13:04:49 +0300

    Update

--- components/sub/ProjectsCard.tsx
+++ components/sub/ProjectsCard.tsx
@@ -10,20 +10,22 @@ interface IProjectsCard {
 
 const ProjectsCard = ({ src, title, description, link }: IProjectsCard) => {
 	return (
-		<div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-[20] inline-block min-w-[256px] min-h-[256px]'>
+		<div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] inline-block min-w-[256px]'>
 			<Link href={link} target='_blank'>
-				<Image
-					src={src}
-					alt={title}
-					width={512}
-					height={512}
-					className='w-full object-cover'
-				/>
+				<div className='flex flex-col'>
+					<Image
+						src={src}
+						alt={title}
+						width={512}
+						height={512}
+						className='w-full object-cover'
+					/>
+					<div className='relative p-4'>
+						<h1 className='text-2xl font-semibold text-white'>{title}</h1>
+						<p className='mt-2 text-gray-300'>{description}</p>
+					</div>
+				</div>
 			</Link>
-			<div className='relative p-4'>
-				<h1 className='text-2xl font-semibold text-white'>{title}</h1>
-				<p className='mt-2 text-gray-300'>{description}</p>
-			</div>
 		</div>
 	)
 }
